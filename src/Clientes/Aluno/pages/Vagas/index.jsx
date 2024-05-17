b6fd7ba5c3aa60d9b77bc81c4ce3570d46394cf4
Bruna Roberta <144182969+brunartt@users.noreply.github.com>
2024-05-16 22:15:06 -0300

    criação dos conteiners da tela de vagas

--- src/Clientes/Aluno/pages/Vagas/index.jsx
+++ src/Clientes/Aluno/pages/Vagas/index.jsx
@@ -1,14 +1,28 @@
+import React from 'react';
 import Footer from '/src/components/Footer/index';
 import Nav from '/src/components/NavLogado/index';
+import './Vagas.css';
 
 function Vagas() {
   return (
     <>
       <Nav />
-      <h1>PAgina de vagas</h1>
+      <div className="vagas-container">
+        <div className="vagas-header">
+          <div className="destaqueVagas">Vagas em destaque</div>
+          <div className="filtro">Filtro</div>
+        </div>
+        
+        <div className="quadros-container">
+          <div className="quadro"></div>
+          <div className="quadro"></div>
+          <div className="quadro"></div>
+          <div className="quadro"></div>
+        </div>
+      </div>
       <Footer />
     </>
-  )
+  );
 }
 
-    export default Vagas;
+export default Vagas;
